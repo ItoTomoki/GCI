22afae4e1361f85ec28d00a6a83b08c5cb64f041
ItoTomoki <tomokiitoupcfax@gmail.com>
2015-09-10 07:21:17 +0900

    Revised

--- niconico/videothreadyomikomiall.py
+++ niconico/videothreadyomikomiall.py
@@ -6,25 +6,27 @@ from ast import literal_eval
 import re
 import MeCab
 import unicodedata
-#f = open('data/tcserv.nii.ac.jp/access/tomokiitoupcfax@gmail.com/832c5b059b15f647/nicocomm/data/thread/0001/sm449.dat')
-#files = os.listdir('data/tcserv.nii.ac.jp/access/tomokiitoupcfax@gmail.com/832c5b059b15f647/nicocomm/data/thread/0001')
-files = os.listdir('data/tcserv.nii.ac.jp/access/tomokiitoupcfax@gmail.com/832c5b059b15f647/nicocomm/data/thread/0001')
+import sys
+
+
+argvs = sys.argv  # コマンドライン引数を格納したリストの取得
+argc = len(argvs) # 引数の個数
+# デバッグプリント
+print argvs[1]
+#print argc
+#ID = '0002'
+ID = str(argvs[1])
+files = os.listdir('data/tcserv.nii.ac.jp/access/tomokiitoupcfax@gmail.com/832c5b059b15f647/nicocomm/data/thread/' + ID)
 thread = {}
-thread["0001"] = {}
+thread[ID] = {}
 kigou = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_abcdefghijklmnopqrstuvwxyz{|}~"
 for nfile in files:
-#for file in files[-50:-1]:
-	#print file
-	#thread["0001"][file] = {}
-	filepass = 'data/tcserv.nii.ac.jp/access/tomokiitoupcfax@gmail.com/832c5b059b15f647/nicocomm/data/thread/0001/' + str(nfile)
+	filepass = ('data/tcserv.nii.ac.jp/access/tomokiitoupcfax@gmail.com/832c5b059b15f647/nicocomm/data/thread/' + ID +'/' +  str(nfile))
 	f = open(filepass)
 	lines2 = f.readlines() # 1行毎にファイル終端まで全て読む(改行文字も含まれる)
 	data1 = f.read()  # ファイル終端まで全て読んだデータを返す
 	f.close()
-	#print type(data1) # 文字列データ
 	Lines2 = {}
-	#lines1 = data1.split('n') # 改行で区切る(改行文字そのものは戻り値のデータには含まれない)
-	#print type(lines2)
 	count = 0
 	for line in lines2:
 		try:
@@ -50,24 +52,24 @@ for nfile in files:
 				print ("Eroor2" + line)
 		#print Lines2[count]['comment']
 		count += 1
-	thread["0001"][nfile] = Lines2
+	thread[ID][nfile] = Lines2
 tagger = MeCab.Tagger( '-Owakati -u /usr/local/Cellar/mecab/0.996/lib/mecab/dic/ipadic/wikipedia-keyword.dic')
 #commentfiles = os.listdir('comment')
-for j in thread["0001"].keys():
-	filename = ("comment0001/" + j[0:-3] +"txt")
+for j in thread[ID].keys():
+	filename = ("comment" + ID + "/" + j[0:-3] +"txt")
 	fo = file(filename,'w')
 	print filename
 	commenttext = ''
-	for i in range(0,len(thread["0001"][j])):
+	for i in range(0,len(thread[ID][j])):
 		if i > 20000:
 			print i,j
 			break
-		commenttext += thread["0001"][j][i]["comment"]
+		commenttext += thread[ID][j][i]["comment"]
 		try:
-			thread["0001"][j][i]["comment"] = unicodedata.normalize('NFKC', thread["0001"][j][i]["comment"])
+			thread[ID][j][i]["comment"] = unicodedata.normalize('NFKC', thread[ID][j][i]["comment"])
 		except:
 			print "normalize Eroor"
-		pluscomment = str(thread["0001"][j][i]["comment"].encode('utf-8'))
+		pluscomment = str(thread[ID][j][i]["comment"].encode('utf-8'))
 		pluscomment = pluscomment.replace("█", "")
 		pluscomment = pluscomment.replace("□", "")
 		pluscomment = pluscomment.replace("※", "")
@@ -85,28 +87,28 @@ for j in thread["0001"].keys():
 		pluscomment = re.sub(re.compile("[!-/:-@[-`{-~]"), '', pluscomment)
 		if pluscomment != '':
 			fo.write(tagger.parse(pluscomment))
-	thread["0001"][j]["comment"] = commenttext
+	thread[ID][j]["comment"] = commenttext
 	fo.close()
 
 files = os.listdir('data/tcserv.nii.ac.jp/access/tomokiitoupcfax@gmail.com/832c5b059b15f647/nicocomm/data/video')
 for nfile in files[1:2]:
 	#print file
-	nfile = "0001.dat"
-	filepass = 'data/tcserv.nii.ac.jp/access/tomokiitoupcfax@gmail.com/832c5b059b15f647/nicocomm/data/video/' + str(nfile)
+	nfile = (ID + ".dat")
+	filepass = ('data/tcserv.nii.ac.jp/access/tomokiitoupcfax@gmail.com/832c5b059b15f647/nicocomm/data/video/' + str(nfile))
 	f = open(filepass)
 	lines2 = f.readlines() # 1行毎にファイル終端まで全て読む(改行文字も含まれる)
-	#data1 = f.read()  # ファイル終端まで全て読んだデータを返す
 	f.close()
 	Lines2 = {}
-	#lines1 = data1.split('n') # 改行で区切る(改行文字そのものは戻り値のデータには含まれない)
-	#print type(lines2)
 	count = 0
 	
 	for line in lines2:
-		Lines2[count] = literal_eval(line)
-		print Lines2[count]["video_id"], Lines2[count]["title"].decode('unicode_escape')
-		thread["0001"][(Lines2[count]["video_id"] + ".dat")]["title"] = Lines2[count]["title"].decode('unicode_escape')
-		count += 1
+		try:
+			Lines2[count] = literal_eval(line)
+			print Lines2[count]["video_id"], Lines2[count]["title"].decode('unicode_escape')
+			thread[ID][(Lines2[count]["video_id"] + ".dat")]["title"] = Lines2[count]["title"].decode('unicode_escape')
+			count += 1
+		except:
+			print line
 
 
 
